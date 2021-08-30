67a11cba39b71120ae896211886ec06d7a0f198b
Serdar Salci <serdarsalci@gmail.com>
2021-08-29 21:09:49 -0400

    Custom User Model

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -29,7 +29,7 @@ class _SignInState extends State<SignIn> {
               print('erros sign in ');
             } else {
               print('signed in ');
-              print(result);
+              print(result.uid);
             }
           },
         ),
--- lib/screens/services/auth.dart
+++ lib/screens/services/auth.dart
@@ -1,14 +1,21 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Create user obj based on FirebaseUser
+
+  UserModel _userFromFirebaseUser(User user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       UserCredential userCredential = await _auth.signInAnonymously();
       User user = userCredential.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
